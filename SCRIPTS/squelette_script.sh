428eecdd9b1f8a838861186a7994c795a8092e70
MathildeCh <mathildecharletb@gmail.com>
2022-10-26 11:31:06 +0200

    ajout du squelette du script pour l'extraction d'urls

---
+++ SCRIPTS/squelette_script.sh
@@ -0,0 +1,20 @@
+#!usr/bin/bash
+
+#ETAPE 1: lecture
+#fichiers de donnees en entree
+#nom du fichier de sortie
+
+#ETAPE 2: pour chaque fichier d'urls
+
+#Lecture du fichier ligne à ligne 
+#pour chaque urls
+#Si URL OK
+#then
+#Si l'encodage est UTF8
+#extraire le tx
+#résultat
+#else
+#...
+#else
+#rien 
+#résultat
