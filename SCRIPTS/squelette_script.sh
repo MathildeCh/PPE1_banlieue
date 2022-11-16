1e1227958b4dfcc923d409baec47d3716034bdeb
MathildeCh <mathildecharletb@gmail.com>
2022-11-16 11:16:05 +0100

    ajout du codeHTTP et charset au squelette

--- SCRIPTS/squelette_script.sh
+++ SCRIPTS/squelette_script.sh
@@ -19,7 +19,7 @@ dirURLs=$1
 ##ETAPE 2: pour chaque fichier d'urls
 for fichier in $(ls $dirURLs)
 do
-    i=0
+    compteur=0
     echo "
     <html>
         <head>
@@ -31,17 +31,23 @@ do
                 <tr>
                     <th>N°</th>
                     <th>URLS $fichier</th>
+                    <th>code HTTP</th>
+                    <th>charset</th>
                 </tr>" > tableau_$fichier.html;
 
     ##Lecture du fichier ligne à ligne
     while read -r line;
     do
+        compteur=$(expr $i + 1)
+        codeHTTP=$(curl -s -L -w '%{http_code}\n' -o ciao.html $line)
+        encodage=$(curl -Is -L -w '%{content_type}\n' $line | grep -i -P -o "charset=\S+" | cut -d= -f2 | head -n1)
         ##pour chaque urls
-        i=$(expr $i + 1)
         echo "
         <tr>
             <td>$i</td>
             <td>$line</td>
+            <td>$codeHTTP</td>
+            <td>$encodage</td>
         </tr>" >> tableau_$fichier.html;
     done < $dirURLs/$fichier;
 
@@ -54,7 +60,7 @@ do
         # if [[ $responseHTTP == 200 OR 3xx ]]
         #then
             ##Si l'encodage est UTF8
-            # if [[ $charset == UTF8 ]]
+            # if [[ $encodage == UTF8 ]]
                 # then
                 ##extraire le tx
                 ##écrie le résultat dans tableau
