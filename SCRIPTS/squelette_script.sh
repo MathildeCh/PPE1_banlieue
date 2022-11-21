3127842372396a9e84ee853a868a47e02b0b0893
DiegoRossini <diego.rossini418@gmail.com>
2022-11-21 19:26:05 +0100

    Ajout if statement pour encodage UTF-8 quand pas de reponse su server + nombre occurrences mot cible

--- SCRIPTS/squelette_script.sh
+++ SCRIPTS/squelette_script.sh
@@ -33,14 +33,37 @@ do
                     <th>URLS $fichier</th>
                     <th>code HTTP</th>
                     <th>charset</th>
+                    <th>Occurrences mot</th>
                 </tr>" > tableau_$fichier.html;
 
     ##Lecture du fichier ligne à ligne
     while read -r line;
     do
+        occurences_mot=$NONE
         compteur=$(($compteur+1))
         codeHTTP=$(curl -s -L -w '%{http_code}\n' -o ciao.html $line)
         encodage=$(curl -Is -L -w '%{content_type}\n' $line | grep -i -P -o "charset=\S+" | cut -d= -f2 | head -n1)
+
+        # Ici on vérifie si un encodage à était recuperé ou pas. Si non, on suppose un encodage en UTF-8
+
+        if test -z "$encodage"
+            then
+            unset encodage
+            encodage=$(echo UTF-8)
+        fi
+
+        # Ici on vérifie si le codeHTTP est valide pour l'extraction et si l'encodage est en UTF-8.
+        # Si oui, on compte les occurrences du mot cible et on affiche sur le tableau (manque le mot en grec!!! LILAS?).
+
+        if [[ $codeHTTP == "200" || $codeHTTP == "302" ]]
+            then
+            if [[ $encodage == "UTF-8" || "uft-8" ]]
+                then
+                unset occurences_mot 
+                occurences_mot=$(lynx -dump -nolist $line | egrep -o -c "\b(suburbs?|periferi(a|e)|banlieues?|περίχωρα)\b")
+            fi 
+        fi
+
         ##pour chaque urls
         echo "
         <tr>
@@ -48,6 +71,7 @@ do
             <td>$line</td>
             <td>$codeHTTP</td>
             <td>$encodage</td>
+            <td>$occurences_mot</td>
         </tr>" >> tableau_$fichier.html;
     done < $dirURLs/$fichier;
 
