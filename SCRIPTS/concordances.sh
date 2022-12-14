01db4e5be5590ee7d69d7b085526c58ffaa1a76b
Lpastre <lilasp@PC-Lilas.localdomain>
2022-12-14 10:10:25 +0100

    Ajouts pour ne pas tenir compte de la casse

--- SCRIPTS/concordances.sh
+++ SCRIPTS/concordances.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/bash
 
 #Pour chaque langue
-for lang in en frc ita gr
+for lang in en fr it gr
 #On cherche la liste des contextes correspondants
 do 
-    liste=$(ls  ../PPE1_banlieue/CONTEXTES/*$lang.txt)
+    liste=$(ls  ./CONTEXTES/*$lang.txt)
     #On crée le concordancier par langue
     echo "
         <html>
@@ -18,20 +18,20 @@ do
                     <th>Contexte droit</th>
                     <th>Mot</th>
                     <th>Contexte gauche</th>
-                </tr>" > ../PPE1_banlieue/CONCORDANCE/Tableau_concordance_$lang.html
+                </tr>" > ./CONCORDANCE/Tableau_concordance_$lang.html
 
     #Pour chaque fichier de la liste 
     for fichier in $liste
         do
             #On cherche les mots correspondants, et on prend les contextes qui précèdent le mot, le mot et les contextes qui les suivent
-             contexte1=$(grep -E -T "suburbs?|periferi[ae]|banlieues?|προ[άα]στι[οαω]ν?" $fichier | sed -E 's/(.*)(suburbs?|periferi[ae]|banlieues?|προ[άα]στι[οαω]ν?)(.*)/<tr><td>\1<\/td><td>\2<\/td><td>\3<\/td><\/tr>/')
+             contexte1=$(grep -E -T -i "suburbs?|periferi[ae]|banlieues?|προ[άα]στι[οαω]ν?" $fichier | sed -E 's/(.*)([sS]uburbs?|[Pp]eriferi[ae]|[Bb]anlieues?|[Ππ]ρο[άα]στι[οαω]ν?)(.*)/<tr><td>\1<\/td><td>\2<\/td><td>\3<\/td><\/tr>/')
              #On stocke directement le tout dans le tableau
-        echo $contexte1 >> ../PPE1_banlieue/CONCORDANCE/Tableau_concordance_$lang.html
+        echo $contexte1 >> ./CONCORDANCE/Tableau_concordance_$lang.html
         done
        
        #On finit le concordancier par langue
         echo "
             </table>
             </body>
-        </html>" >> ../PPE1_banlieue/CONCORDANCE/Tableau_concordance_$lang.html
+        </html>" >> ./CONCORDANCE/Tableau_concordance_$lang.html
 done
